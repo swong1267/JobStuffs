a2735c80e1513ad558aa064c3610424df39fad79
haiyinwang <haiyinwangh@gmail.com>
2016-06-01 10:55:42 -0700

    Initial Chart Set Up

--- js/main.js
+++ js/main.js
@@ -1,44 +1,32 @@
 /*jshint esversion: 6 */
 (function() {
 
+    var myJob = "Software Engineer";
+    var mySalary = 90000;
+
     var ctx = $("#companyChart");
-    var myChart = new Chart(ctx, {
-                type: 'bar',
-                data: {
-        labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
-        datasets: [{
-            label: '# of Votes',
-            data: [12, 19, 3, 5, 2, 3],
-            backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)'
-            ],
-            borderColor: [
-                'rgba(255,99,132,1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
-            ],
-            borderWidth: 1
-        }]
-    },
-                options: {
-                    responsive: true,
-                    responsiveAnimationDuration: 0,
-                    maintainAspectRatio: true
-                }
-    });
 
     // Load Company Pay Data
     d3.csv("data/company_pay.csv", function(data) {
         if(data) {
-
+            var myChart = new Chart(ctx, {
+                        type: 'bar',
+                        data: {
+                            labels: [myJob, data[1].Name, data[2].Name],
+                            datasets: [{
+                                label: 'Median Salary',
+                                data: [mySalary, data[1].Pay, data[2].Pay],
+                                backgroundColor: 'rgba(255, 99, 132, 0.2)',
+                                borderColor: 'rgba(255,99,132,1)',
+                                borderWidth: 1
+                            }]
+                        },
+                        options: {
+                            responsive: true,
+                            responsiveAnimationDuration: 0,
+                            maintainAspectRatio: true
+                        }
+            });
         } else {
             console.log("Company Data Loading Error");
         }
